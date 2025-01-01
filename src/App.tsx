50b87f93efc13d77a1ec0ddcd449a3ca33819c9a
GauravVKulkarni <gauravvilaskulkarni9211@gmail.com>
2025-01-02 03:39:58 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
+import Projects from './components/pages/Projects';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
-    <>
-      <div className='main-container'>
+    <Router>
+      <div className="main-container">
         <Navbar />
-        <div className='container'>
-
+        <div className="container">
+          {/* Render different page components based on the route */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
         </div>
         <Footer />
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
 export default App
